e4a7b670e94599ab42d348cfa2e28afa06af96af
Vaibhav <vaibhavagrawal093@gmail.com>
2021-10-10 21:24:19 +0530

    update9.9

--- OneStop/frontend/src/component/dashboard/Dashboard.js
+++ OneStop/frontend/src/component/dashboard/Dashboard.js
@@ -2,8 +2,10 @@ import React, { Fragment, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getCurrentProfile } from '../../actions/profile';
+import { getCurrentProfile,deleteAccount } from '../../actions/profile';
 import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 
 const Dashboard = ({getCurrentProfile, auth: {user}, profile: {profile} }) => {
   useEffect(() => {
@@ -20,6 +22,14 @@ const Dashboard = ({getCurrentProfile, auth: {user}, profile: {profile} }) => {
         { profile !== null ? (
           <Fragment>
            <DashboardActions />
+           <Experience experience={profile.experience} />
+            <Education education={profile.education} />
+
+            <div className="my-2">
+              <button className="btn btn-danger" onClick={() => deleteAccount()}>
+                <i className="fas fa-user-minus" /> Delete My Account
+              </button>
+            </div>
           </Fragment>
         ) : (
           <Fragment>
--- OneStop/frontend/src/reducers/auth.js
+++ OneStop/frontend/src/reducers/auth.js
@@ -6,6 +6,7 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
+  ACCOUNT_DELETED
 } from '../actions/types';
 
 // token is stored in local storage using Javascript  
@@ -43,6 +44,7 @@ function authReducer(state = initialState, action) {
     case LOGIN_FAIL:
     case AUTH_ERROR:
     case LOGOUT:
+    case ACCOUNT_DELETED:
       // localStorage.removeItem('token');
       return {
         ...state,
