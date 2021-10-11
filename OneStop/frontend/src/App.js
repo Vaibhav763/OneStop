2afcf6a254f59f6bc1136d04674da219dde89863
Vaibhav <vaibhavagrawal093@gmail.com>
2021-10-11 12:18:59 +0530

    update10.6

--- OneStop/frontend/src/App.js
+++ OneStop/frontend/src/App.js
@@ -10,6 +10,7 @@ import PrivateRoute from './component/routing/PrivateRoute';
 import AddExperience from './component/profile_forms/AddExperience';
 import AddEducation from './component/profile_forms/AddEducation';
 import Profiles from './component/profiles/Profiles';
+import Profile from './component/profile/Profile';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from './store'
@@ -38,6 +39,7 @@ const App = () => {
         <Route exact path='/login'> <Login/></Route>
         <Route exact path='/register'> <Register/></Route>
         <Route exact path='/profiles'> <Profiles/></Route>
+        <Route exact path='/profile/:id'> <Profile/></Route>
         <PrivateRoute exact path='/dashboard' component={Dashboard}></PrivateRoute>
         <PrivateRoute exact path='/create_profile' component={ProfileForm}></PrivateRoute>
         <PrivateRoute exact path='/add_experience' component={AddExperience}></PrivateRoute>
